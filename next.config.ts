b5c25876fc922dd0a8c103261a6d52d668a47a20
commit 1 <f>
2026-06-06 02:59:30 +0200

    d

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   serverExternalPackages: ['prettier'],
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true,
   },
   experimental: {
     serverActions: {
